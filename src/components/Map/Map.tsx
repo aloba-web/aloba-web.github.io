c011cdf6d556aa7349ad9bad1daca675e671814b
Samir Chaabi <samir.chaabi@outlook.com>
2021-04-21 19:57:00 +0200

    disable one finger dragging on mobile for the map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -2,13 +2,15 @@ import { LatLngBoundsExpression, LatLngTuple } from "leaflet";
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Store } from "../../hooks/fetch/fetchTypes";
+import useWindowSize from "../../hooks/useWindowSize";
 import "./Map.scss";
 
 export interface MapProps {
   stores: Array<Store>;
 }
 
-const Map: React.FC<MapProps> = ({stores}) => {
+const Map: React.FC<MapProps> = ({ stores }) => {
+  const { isMobile } = useWindowSize();
   const bounds: LatLngBoundsExpression = [];
   const getMarkers = stores.map(
     ({ coordinates: { lat, lng }, storeName, address, url }) => {
@@ -35,9 +37,11 @@ const Map: React.FC<MapProps> = ({stores}) => {
       attributionControl={false}
       bounds={bounds}
       boundsOptions={{ padding: [10, 10] }}
+      dragging={!isMobile}
       zoom={5}
       scrollWheelZoom={false}
       style={{ height: 400, width: "100%" }}
+      touchZoom={true}
     >
       <TileLayer url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png" />
       {getMarkers}
